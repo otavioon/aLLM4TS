cf8f7b6ed3d8c81df55acb05469f147301b2c878
Otavio Napoli <otavio.napoli@gmail.com>
2025-02-17 12:25:59 +0000

    Added scripts to run TFC-A, TFC-AE and TFC-AD experiments
    Signed-off-by: Otavio Napoli <otavio.napoli@gmail.com>

---
+++ run_experiment_tfc_A.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+
+CKPT_DIR="./checkpoints/experiments/TFC-A/ETTh1_ETTm1_ETTh2_ETTm2_weather_traffic_electricity_illness/"
+MODEL_NAME="tfc-transformer-encoder"
+CKPT_VERSION="final"
+FINAL_DIR=${CKPT_DIR}/${MODEL_NAME}/${CKPT_VERSION}
+
+mkdir -p ${FINAL_DIR}
+
+
+python run_tfc_pretrain.py   \
+    --checkpoints ${CKPT_DIR} \
+    --model_name ${MODEL_NAME} \
+    --ckpt_version ${CKPT_VERSION} \
+    --input_channels 1 \
+    --TS_length 1024 \
+    --batch_size 128 \
+    --num_workers 0 \
+    --data pretrain \
+    --train_epochs 100 \
+    --accelerator gpu \
+    --devices 1 2>&1 | tee ${CKPT_DIR}/log.txt
