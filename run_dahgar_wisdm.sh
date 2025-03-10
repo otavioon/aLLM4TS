79dcbe401e1effb5f199536de3e38d4d2ee2f7c5
Otavio Napoli <otavio.napoli@gmail.com>
2025-03-10 13:39:18 +0000

    Updated finetune scripts to load from last modified ckpt

--- run_dahgar_wisdm.sh
+++ run_dahgar_wisdm.sh
@@ -29,8 +29,9 @@ for patch in 8 ; do
             --learning_rate 0.002 \
             --random_seed 2021 \
             --pt_sft 1 \
-            --pt_sft_base_dir ./checkpoints/pt_patch/ETTh1_ETTm1_ETTh2_ETTm2_weather_traffic_electricity_illness/ \
+            --pt_sft_base_dir ./checkpoints/experiments/aLLM4TS-A/ETTh1_ETTm1_ETTh2_ETTm2_weather_traffic_electricity_illness \
             --pt_sft_model pretrain_LLM4TS_pt_sl1024_pl1024_llml6_lr0.0001_bs256_percent100_ln_wpe_attn_mlp_gpt2_w_weight_s16_0 \
+            --load_last \
             --sft 1 \
             --sft_layers ln_wpe_attn_mlp \
             --checkpoints ./checkpoints/classification/WISDM \
@@ -39,6 +40,6 @@ for patch in 8 ; do
             --use_gpu 1 \
             --devices 0 \
             --gpu 0 \
-            --num_workers 0 2>&1 | tee logs/classification/WISDM_patch-${patch}_stride-${stride}.log 
+            --num_workers 0 2>&1 | tee logs/classification/DAGHAR-WISDM_patch-${patch}_stride-${stride}.log 
     done
 done
